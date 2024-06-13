4b19bae43fd7c26d595f1711215c496c938adaa9
chiragdcmarix <77272627+chiragdcmarix@users.noreply.github.com>
2024-06-13 18:12:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
 });
